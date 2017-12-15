8afa50abb1fb2f23d87916d5757f8e450876b6c7
Ubuntu <ubuntu@ip-172-31-32-28.eu-central-1.compute.internal>
2017-12-15 15:52:20 +0000

    Added Homework 6

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Install Ruby
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+# Install MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list' 
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+# Deploy
+git clone https://github.com/Otus-DevOps-2017-11/reddit.git
+cd reddit && bundle install
+puma -d
