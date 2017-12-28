1cdf931bdaea320c25392cca386505f0025f5938
Vadim Myagkov <g0nz0@mail.ru>
2017-12-28 15:32:00 +0000

    Homework 8 second extra task

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,10 @@
 output "app_external_ip" {
   value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
 }
+output "app_external_ip2" {
+  value = "${google_compute_instance.app2.network_interface.0.access_config.0.assigned_nat_ip}"
+}
+
+output "lb_external_ip" {
+  value = "${google_compute_global_forwarding_rule.app_rule.ip_address}"
+}
